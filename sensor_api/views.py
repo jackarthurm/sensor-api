53d9a338672576ff055a54c627658a93417a9332
Jack Mann <jack.mann@hexagon.com>
2022-07-18 01:28:58 +0100

    used CreateAPIView to simplify code

--- sensor_api/views.py
+++ sensor_api/views.py
@@ -1,9 +1,10 @@
-from typing import List, Dict
-
 from django_filters import CharFilter
 from django_filters.rest_framework import FilterSet
 from rest_framework import status
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+)
 from rest_framework.mixins import (
     ListModelMixin,
     CreateModelMixin,
@@ -37,32 +38,20 @@ class ContinuousSensorFilterSet(FilterSet):
     sensor = CharFilter(field_name="sensor__name", lookup_expr='iexact')
 
 
-class ContinuousSensorMeasurementView(ListModelMixin, GenericAPIView):
+class ContinuousSensorMeasurementView(ListAPIView, CreateAPIView):
 
     queryset = ContinuousSensorMeasurement.objects.all()
     serializer_class = ContinuousSensorMeasurementSerializer
     filterset_class = ContinuousSensorFilterSet
 
-    def get(self, request: Request, *args, **kwargs) -> Response:
-        return self.list(request, *args, **kwargs)
-
     def patch(self, request: Request) -> Response:
+        """Creates a list of sensor resources"""
 
-        bulk_data: List[Dict] = request.data.get("bulk_data", [])
-
-        sz = self.get_serializer(data=bulk_data, many=True)
+        sz = self.get_serializer(data=request.data, many=True)
         sz.is_valid(raise_exception=True)
         sz.save()
 
         return Response(
             status=status.HTTP_201_CREATED,
-            data={"bulk_data": sz.data}
+            data=sz.data
         )
-
-    def post(self, request: Request) -> Response:
-
-        sz = self.get_serializer(data=request.data)
-        sz.is_valid(raise_exception=True)
-        sz.save()
-
-        return Response(status=status.HTTP_201_CREATED, data=sz.data)
